19134142aaad467c90bd835782bd8fb7f9910ca0
Sfayter <charlie.fay21@gmail.com>
2024-09-16 19:20:47 -0300

    Teste Work

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const servidor = express();
 
 servidor.get('/helloword', (req, response) => {
     //código do endpoint
-    response.send('Olá! API subiu com sucesso. Booooa! :)');
+    response.send('Olá! API subiu com sucesso. Boa! :)');
 })
 
 servidor.listen(5001, () => console.log('API subiu! Parabéns.'));
