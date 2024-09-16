9212325acaeaf6245c12adf423088d6d43668450
CharlieCernov <charlie.fay21@gmail.com>
2024-09-16 13:11:03 -0300

    Endpoint

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-console.log('Olá! Em breve serei uma API.')
+import express from 'express';
 
+const servidor = express();
+
+servidor.get('/helloword', (req, response) => {
+    //código do endpoint
+    response.send('Olá! API subiu com sucesso. Booooa! :)');
+})
+
+servidor.listen(5001, () => console.log('API subiu! Parabéns.'));
 
